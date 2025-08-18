da540ad7b17f8c693a5683c13a201c0bb43c0684
Nanairo <nepinepi.mofumofu@gmail.com>
2025-08-18 21:26:06 +0900

    自分用リポジトリにアップ

---
+++ fuel/app/logs/2025/08/18.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-08-18 21:17:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
